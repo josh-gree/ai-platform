8df711591c26f13bc2aa6fba0f006138f7b8863a
josh-gree <joshua.greenhalgh@heyjobs.de>
2019-10-12 19:13:06 +0200

    package setup.py

---
+++ model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="model",
+    version="0.1",
+    author="joshgree",
+    author_email="joshuadouglasgreenhalgh@gmail.com",
+    packages=["model"],
+    zip_safe=False,
+)
